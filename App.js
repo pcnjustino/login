08c96bf2a569b8f6de94d1d5263ea0440de2790d
pcnjustino <paulo.njustino@gmail.com>
2024-06-23 19:02:52 -0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View ,SafeAreaView,Pressable} from 'react-native';
 
-export default function App() {
+export default function App(){
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={[styles.container,{width:"100%"}]}>
+        <TextInput placeholder='EordMAIL OR USERNAME' style={styles.input}/>
+        <TextInput 
+        autoCorrect={false} 
+        placeholder='PASSWORD'
+        name="password" 
+        autoCapitalize='none'
+        secureTextEntry="true"
+        style={styles.input}/>
+        <Pressable style={styles.button} onPress={()=>{alert("login success")}}>
+              <Text style={styles.buttonText}>Login</Text>
+        </Pressable>
+      </View>  
+      <StatusBar style="auto"/>
+    </SafeAreaView>
   );
 }
 
@@ -17,4 +29,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+  input:{
+      width:"60%",
+      marginTop:20,
+      height:56,
+      borderWidth:0.5,
+      borderRadius:7,
+      borderColor:"#777070",
+      paddingHorizontal:20,
+  },
+  button:{
+    backgroundColor : "red",
+    width:"56%",
+    height : 45,
+    //borderColor : "gray",
+    //borderWidth  : 1,
+    borderRadius : 7,
+    marginTop:42,
+    alignItems : "center",
+    justifyContent : "center",
+  },
+  buttonText : {
+    color : "white"  ,
+    fontSize: 18,
+    fontWeight : "bold"
+  }, 
+  
+})
